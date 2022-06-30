70c19c0a8209ad2f47742ca989801f55f9fff7cc
QiuyuLian <qiuyu_lian@163.com>
2022-06-30 11:21:36 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'TOMAS'
+copyright = '2022, Qiuyu Lian'
+author = 'Qiuyu Lian'
 
 release = '0.1'
 version = '0.1.0'
