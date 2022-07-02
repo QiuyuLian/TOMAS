907b110b128a17d541003d14cbcc932b4fb236bb
QiuyuLian <qiuyu_lian@163.com>
2022-07-02 23:31:40 +0800

    test

--- tomas/lumache.py
+++ tomas/lumache.py
@@ -27,6 +27,24 @@ def initialize_alpha(Y=1):
     :return: 1
     :rtype: int
     """
-    Y = np.zeros(1)
+    gene_tot = np.ravel(np.sum(Y, axis=0))
+    lib_size = np.ravel(np.sum(Y, axis=1))
     
-    return 1
+    G = sum(gene_tot > 0)
+    #C = sum(lib_size > 0)
+    
+    new_Y = Y[lib_size > 0, :]
+    new_Y = new_Y[:, gene_tot > 0] 
+    
+    p_mat = ( new_Y.T/lib_size[lib_size > 0] ).T
+    p_mean = p_mat.mean(0)
+    p_var = p_mat.var(0)
+    p_var[p_var == 0] = np.nanmean(p_var)
+    
+    alpha_sum = np.exp( sum(np.log(p_mean[:-1]*(1-p_mean[:-1])/p_var[:-1] - 1))/(G-1) )
+    
+    alpha = np.array([0.0]*Y.shape[1])
+    alpha[gene_tot > 0] = alpha_sum * p_mean
+    alpha[alpha == 0.0] = 1e-6
+    
+    return 10
